0af62c1a8a7b1852cb2ee654e9f71237c0a446bb
temkin1982 <124079603+temkin1982@users.noreply.github.com>
2023-02-18 23:27:52 +0200

    HomeWork

---
+++ PracticHome_006/Program.cs
@@ -0,0 +1,114 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PracticHome_006
+{
+    internal class Program
+    {
+        
+
+
+
+        static void Main(string[] args)
+        {
+            // 2
+
+            //string line = "Hello world! I love learning programming";
+            //string[] words = line.Split(" ");
+            //string wordMax = "";
+
+            //foreach (var itemMax in words)
+            //{
+            //    if (itemMax.Length > 0)
+            //    {
+            //        wordMax = itemMax;
+
+            //    }
+            //}
+            //Console.WriteLine(words.Min());
+            //Console.WriteLine(wordMax);
+            //Console.ReadKey();
+
+            //3
+
+            //string line = "ffffff ab 1234 jkjk";
+            //string[] words = line.Split(' ');
+            //string resultWord = words[0];
+
+            //for (int i = 0; i < words.Length; i++)
+            //{
+            //    Console.WriteLine(words[i].Distinct().Count());
+            //    Console.WriteLine(resultWord.Distinct().Count());
+            //    Console.WriteLine("-----------------------------");
+
+            //    if (words[i].Distinct().Count() < resultWord.Distinct().Count()) resultWord = words[i];
+            //}
+            //Console.WriteLine(resultWord);
+            //Console.ReadKey();
+
+            //5
+
+            //string line = "Hello";
+            //string[] word = line.Split(' ');
+            //string newWord = " ";
+            //int count = 0;
+            //for (int i = 0; i < word.Length; i++)
+            //{
+            //    var str = word[i].Trim();
+            //    if (word.Length >= count)
+            //    {
+            //        word[i] = $"{str[0]}{str[0]}{str[1]}{str[1]}{str[2]}{str[2]}{str[3]}{str[3]}{str[4]}{str[4]}";
+            //        count++;
+
+            //    }
+
+            //}
+            //Console.WriteLine(String.Join(" ", word));
+            //Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
